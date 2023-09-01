b2828260c6c2cd189732751150f1988c2a836eab
llmosquera <105768153+llmosquera@users.noreply.github.com>
2023-08-31 19:22:28 -0500

    Update ICursoDAO.java

--- src/main/java/com/espe/dao/ICursoDAO.java
+++ src/main/java/com/espe/dao/ICursoDAO.java
@@ -1,22 +1,23 @@
-package com.espe.dao;
 
+package com.espe.dao;
+//Importamos todas las librerias
 import com.espe.model.Curso;
 
 import java.util.List;
 
 public interface ICursoDAO {
-    // Método para guardar un curso en la base de datos
+    // Método para guardar un curso en la BDD
     void guardar(Curso curso);
 
-    // Método para editar la información de un curso en la base de datos
+    // Método para editar la información de un curso en la BDD
     void editar(Curso curso);
 
-    // Método para buscar un curso por su ID en la base de datos
+    // Método para buscar un curso por su ID en BDD
     Curso buscar(int id);
 
-    // Método para obtener una lista de todos los cursos en la base de datos
+    // Método para obtener una lista de todos los cursos en la BDD
     List<Curso> obtenerCursos();
 
-    // Método para eliminar un curso por su ID de la base de datos
+    // Método para eliminar un curso por su ID de la BDD
     void eliminar(int id);
 }
